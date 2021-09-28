158483f926c8d319d45ac0adcb5527695925327f
hongchhe <hehongchuang@hotmail.com>
2021-09-28 22:08:59 +0800

    fix

--- load-from-file/externalCall.groovy
+++ load-from-file/externalCall.groovy
@@ -1,7 +1,7 @@
 // If there's a call method, you can just load the file, say, as "foo", and then invoke that call method with foo(...) 
 def call(String whoAreYou) {
     echo "Now we're being called more magically, ${whoAreYou}, thanks to the call(...) method."
-    test1 = ${whoAreYou}
+    test1 = whoAreYou
     println("test1: ${test1}, whoAreYou: ${whoAreYou}")
     sleep 3
     println("test1: ${test1}, whoAreYou: ${whoAreYou}")
--- load-from-file/externalMethod.groovy
+++ load-from-file/externalMethod.groovy
@@ -1,10 +1,10 @@
 // Methods in this file will end up as object methods on the object that load returns.
 def lookAtThis(String whoAreYou) {
     echo "Look at this, ${whoAreYou}! You loaded this from another file!"
-    test1 = ${whoAreYou}
-    echo "test1: ${test1}, whoAreYou: ${whoAreYou}"
+    test1 = whoAreYou
+    println("test1: ${test1}, whoAreYou: ${whoAreYou}")
     sleep 3
-    echo "test1: ${test1}, whoAreYou: ${whoAreYou}"
+    println("test1: ${test1}, whoAreYou: ${whoAreYou}")
 }
 
 return this;

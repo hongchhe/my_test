64fd715572f4813d719615efa0eaaee42ce04247
hongchhe <hehongchuang@hotmail.com>
2021-09-29 22:39:37 +0800

    update

--- load-from-file/externalCall.groovy
+++ load-from-file/externalCall.groovy
@@ -3,8 +3,14 @@ def call(String whoAreYou) {
     echo "Now we're being called more magically, ${whoAreYou}, thanks to the call(...) method."
     test1 = whoAreYou
     println("test1: ${test1}, whoAreYou: ${whoAreYou}")
-    sleep 3
+    sleep 10
     println("test1: ${test1}, whoAreYou: ${whoAreYou}")
+    testfunc1("test_func_1")
 }
 
 return this;
+
+def testfunc1(String s1) {
+    echo "testfunc: ${s1};"
+    println("testfunc: ${s1};")
+}
--- load-from-file/externalMethod.groovy
+++ load-from-file/externalMethod.groovy
@@ -3,8 +3,14 @@ def lookAtThis(String whoAreYou) {
     echo "Look at this, ${whoAreYou}! You loaded this from another file!"
     test1 = whoAreYou
     println("test1: ${test1}, whoAreYou: ${whoAreYou}")
-    sleep 3
+    sleep 10
     println("test1: ${test1}, whoAreYou: ${whoAreYou}")
+    testfunc2("test_func_2")
 }
 
 return this;
+
+def testfunc2(String s1) {
+    echo "testfunc: ${s1};"
+    println("testfunc: ${s1};")
+}

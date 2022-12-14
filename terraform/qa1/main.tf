9b4d3bd6add2679fd4dd2f83d569b38a97ca48a2
Adam He <hehongchuang@hotmail.com>
2022-12-14 18:08:49 +0800

    add more

---
+++ terraform/qa1/main.tf
@@ -0,0 +1,21 @@
+resource "null_resource" "example2" {
+}
+
+# terraform {
+#   required_providers {
+#     ibm = {
+#       source = "IBM-Cloud/ibm"
+#       version = ">= 1.12.0"
+#     }
+#   }
+# }
+
+# # Configure the IBM Provider
+# provider "ibm" {
+#   region = "us-south"
+# }
+
+# Create a VPC
+# resource "ibm_is_vpc" "testacc_vpc" {
+#   name = "test-vpc"
+# }

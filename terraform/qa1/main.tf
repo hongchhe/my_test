9d13107e9f26cf0e393cd32f1acc4c5de0093aec
Adam He <hehongchuang@hotmail.com>
2023-01-13 17:03:54 +0800

    test

--- terraform/qa1/main.tf
+++ terraform/qa1/main.tf
@@ -1,6 +1,48 @@
 resource "null_resource" "example2" {
 }
 
+locals {
+  map1 = {
+    item1 = {
+      name1 = "item1value1"
+      name2 = "item1value2"
+    }
+    item2 = {
+      name1 = "item2value1"
+      name2 = "item2value2"
+    }
+  }
+}
+
+resource "null_resource" "null_resource_simple" {
+  for_each = local.map1
+  provisioner "local-exec" {
+    command = "echo ${each.key} ${each.value.name1} ${each.value.name2}"
+  }
+}
+
+resource "null_resource" "before" {
+  provisioner "local-exec" {
+    command = "echo \"Wait 60 seconds\""
+  }
+}
+
+resource "null_resource" "delay" {
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+  triggers = {
+    "before" = "${null_resource.before.id}"
+  }
+}
+
+resource "null_resource" "after" {
+  provisioner "local-exec" {
+    command = "echo \"Finish\" "
+  }
+  depends_on = [null_resource.delay]
+}
+
 # terraform {
 #   required_providers {
 #     ibm = {
